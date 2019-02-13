1c1ae98d322e6163c5c580fa868a42bca8caaa10
pietvanzoen <hi@pietvanzoen.com>
2019-02-13 08:50:57 +0100

    use env var for updates

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,7 @@ import fetch from 'isomorphic-unfetch';
 class Page extends React.Component {
   static async getInitialProps() {
     try {
-      const res = await fetch(
-        'https://raw.githubusercontent.com/pietvanzoen/updates/master/updates.json',
-      );
+      const res = await fetch(process.env.UPDATES_API_URL);
       const updates = await res.json();
       return updates;
     } catch (e) {
