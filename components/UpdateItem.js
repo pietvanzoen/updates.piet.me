5f46f015d6e9dae5bbd938b1e0218d2fad858f96
pietvanzoen <hi@pietvanzoen.com>
2019-02-18 21:06:45 +0100

    relative date

--- components/UpdateItem.js
+++ components/UpdateItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { distanceInWordsStrict } from 'date-fns';
 
 function UpdateItem({ update }) {
   const { data, content } = update;
   return (
     <article>
       <div dangerouslySetInnerHTML={{ __html: content.html }} />
-
-      <time>{data.date}</time>
+      <time>
+        <small>
+          {distanceInWordsStrict(data.date, new Date())}
+          {' '}
+ago
+        </small>
+      </time>
       <ul>
         {data.twitter && (
           <li>
