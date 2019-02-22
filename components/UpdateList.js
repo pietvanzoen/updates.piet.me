d68f657198dffd67dac9986c8707b32a825067bc
pietvanzoen <hi@pietvanzoen.com>
2019-02-22 16:09:56 +0100

    show and hide replies

--- components/UpdateList.js
+++ components/UpdateList.js
@@ -1,11 +1,28 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import UpdateItem from './UpdateItem';
 
 function UpdateList({ updates }) {
+  const [showReplies, setShowReplies] = useState(false);
+  const filteredUpdates = showReplies
+    ? updates
+    : updates.filter(update => !update.data.in_reply_to);
+
   return (
     <div>
-      {updates.map(update => (
+      <form className="text-right text-small pure-form">
+        <label className="pure-checkbox" htmlFor="replies">
+          <input
+            type="checkbox"
+            id="replies"
+            checked={showReplies}
+            onChange={event => setShowReplies(event.target.checked)}
+          />
+          {' '}
+          Show replies
+        </label>
+      </form>
+      {filteredUpdates.map(update => (
         <UpdateItem key={update.path} update={update} />
       ))}
     </div>
