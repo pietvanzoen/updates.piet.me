dea878a17cc5d626df453a9e3000d4ea7b89c2e1
pietvanzoen <hi@pietvanzoen.com>
2019-02-22 17:12:41 +0100

    functions are hoisted duh

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     'react/jsx-filename-extension': 'off',
-    'no-param-reassign': 'off'
+    'no-param-reassign': 'off',
+    'no-use-before-define': ['error', 'nofunc'] // functions are hoisted
   },
   env: {
     browser: true
