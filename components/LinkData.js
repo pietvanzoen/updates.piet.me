7df4de60fc49a60da791ede70294ce6a9de96095
pietvanzoen <hi@pietvanzoen.com>
2019-02-23 22:50:27 +0100

    show domain on link card

--- components/LinkData.js
+++ components/LinkData.js
@@ -3,22 +3,19 @@ import PropTypes from 'prop-types';
 
 function LinkData({ data }) {
   const {
-    ogUrl,
-    ogTitle,
-    ogDescription,
-    ogImage,
-    ogSiteName,
-    twitterCreator,
+    ogUrl, ogTitle, ogDescription, ogImage,
   } = data;
   if (!ogUrl) return '';
+  const domain = ogUrl
+    .replace('http://', '')
+    .replace('https://', '')
+    .split(/[/?#]/)[0];
   return (
     <a href={ogUrl} className="link-box pure-g">
       <div className="pure-u-sm-3-4 link-box-content pure-u-1">
         <h3 className="link-box-title">{`${ogTitle}`}</h3>
         <p>{ogDescription}</p>
-        {twitterCreator && (
-          <a href={`https://twitter.com/${twitterCreator}`}>{twitterCreator}</a>
-        )}
+        <span className="text-accent">{domain}</span>
       </div>
       {ogImage && ogImage.url && (
         <div className="pure-u-sm-1-4 pure-u-1 link-box-img">
